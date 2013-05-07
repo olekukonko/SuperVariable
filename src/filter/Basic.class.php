5d4d2f1e891388fe20134388f15b33fc941b7084
olekukonko <github.com@12>
2013-05-07 18:19:45 +0100

    Added Support for Ignore

--- src/filter/Basic.class.php
+++ src/filter/Basic.class.php
@@ -12,6 +12,10 @@ namespace super\filter;
 use super\filter\Parsable;
 
 class Basic implements Parsable {
+	
+	/**
+	 * FILTER CONSTANTS
+	 */
 	const FILTER_NONE = 1;
 	const FILTER_XSS = 2;
 	const FILTER_SQL = 4;
@@ -19,11 +23,25 @@ class Basic implements Parsable {
 	const FILTER_LOW = 16;
 	const FILTER_HIGH_LOW = 32;
 	const FILTER_INT = 64;
+	const FILTER_FLOAT = 128;
 	const FILTER_ALL = 63;
+	/**
+	 * IGNORE CONSTANTS
+	 */
+	const IGNORE_HEX = 1;
+	const IGNORE_BASE64 = 2;
+	const IGNORE_BINARY = 4; // use carefully can be exploited
 	private $flags;
+	private $ignore;
 
-	function __construct($flags = Basic::FILTER_NONE) {
+	/**
+	 *
+	 * @param int $flags
+	 * @param int $ignore
+	 */
+	function __construct($flags = Basic::FILTER_NONE, $ignore = 0) {
 		$this->flags = $flags;
+		$this->ignore = $ignore;
 		// var_dump(1 | 2 | 4 | 8 | 16 | 32 , 63 & 63);
 	}
 
@@ -55,16 +73,43 @@ class Basic implements Parsable {
 	function parse($key, $mixed) {
 		if (is_string($mixed)) {
 			
+			/**
+			 * Only work in printable string
+			 * Note : This can be exploited use carefully
+			 * "asdf\n\r\t" would been seen as binary because not all characters
+			 * are printable but
+			 * "LKA#@%.54" would be seen as normal string;
+			 */
+			if (($this->ignore & self::IGNORE_BINARY) && ! ctype_print($mixed)) {
+				return $mixed;
+			}
+			
+			// Check if its Hex md5, sha1 etc all use hex
+			if (($this->ignore & self::IGNORE_HEX) && ctype_xdigit($mixed)) {
+				return $mixed;
+			}
+			
+			// You can also ignore base 64 images
+			if (($this->ignore & self::IGNORE_BASE64) && base64_encode(base64_decode($mixed)) === $mixed) {
+				return $mixed;
+			}
+			
 			// var_dump($this->flags & self::FILTER_XSS);
 			// var_dump($this->flags & self::FILTER_LOW);
 			// var_dump($this->flags & self::FILTER_INT);
 			
+			// General Filters
 			$this->flags & self::FILTER_XSS and $mixed = htmlspecialchars($mixed, ENT_QUOTES, 'UTF-8');
 			$this->flags & self::FILTER_SQL and $mixed = $this->escape($mixed);
+			
+			// Working with Strings
 			$this->flags & self::FILTER_HIGH and $mixed = filter_var($mixed, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_HIGH);
 			$this->flags & self::FILTER_LOW and $mixed = filter_var($mixed, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
 			$this->flags & self::FILTER_HIGH_LOW and $mixed = filter_var($mixed, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_LOW);
+			
+			// Working with numbers
 			$this->flags & self::FILTER_INT and $mixed = filter_var($mixed, FILTER_SANITIZE_NUMBER_INT);
+			$this->flags & self::FILTER_FLOAT and $mixed = filter_var($mixed, FILTER_SANITIZE_NUMBER_FLOAT);
 			
 			return $mixed;
 		}
